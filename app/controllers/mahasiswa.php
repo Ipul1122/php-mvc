58a5605ac6d8ec81ea3a6637fe04d8afd1809a28
Syaiful ipul <127600767+Ipul1122@users.noreply.github.com>
2025-01-23 01:30:40 +0700

    Update mahasiswa.php

--- app/controllers/mahasiswa.php
+++ app/controllers/mahasiswa.php
@@ -10,4 +10,13 @@ class Mahasiswa extends Controller{
         $this->view('templates/footer'); //folder views mencari file templates/footer
         
     }
+    public function detail($id)
+    {
+        $data['judul'] = 'Detail Mahasiswa'; //membuat array judul dengan isi Daftar Mahasiswa
+        $data['mhs'] = $this->model('Mahasiswa_model')->getMahasiswaById($id); //membuat array mhs dengan isi model Mahasiswa_model dan method getMahasiswaById
+        $this->view('templates/header' , $data); //folder views mencari file templates/header
+        $this->view('mahasiswa/detail', $data);  //folder views mencari file home/index
+        $this->view('templates/footer'); //folder views mencari file templates/footer
+        
+    }
 }    
